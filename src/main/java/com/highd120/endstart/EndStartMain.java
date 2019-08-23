aca10c9765bece7ca4dd4f5a9cdffe2b8565a80c
highd <hdgames123@yahoo.co.jp>
2019-08-23 23:27:28 +0900

    レシピ削除用のコマンド追加

--- src/main/java/com/highd120/endstart/EndStartMain.java
+++ src/main/java/com/highd120/endstart/EndStartMain.java
@@ -1,5 +1,6 @@
 package com.highd120.endstart;
 
+import com.highd120.endstart.command.DeleteRecipeTmpCommand;
 import com.highd120.endstart.item.ItemRecipeCreater;
 import com.highd120.endstart.util.block.BlockManager;
 import com.highd120.endstart.util.item.ItemManager;
@@ -17,31 +18,31 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
  */
 @Mod(modid = EndStartMain.MOD_ID, version = EndStartMain.VERSION)
 public class EndStartMain {
-    public static final String MOD_ID = "endstart";
-    public static final String MOD_NAME = "End Start";
-    public static final String VERSION = "1.10.2";
+	public static final String MOD_ID = "endstart";
+	public static final String MOD_NAME = "End Start";
+	public static final String VERSION = "1.10.2";
 
-    @Instance
-    public static EndStartMain instance = new EndStartMain();
+	@Instance
+	public static EndStartMain instance = new EndStartMain();
 
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-        ItemRecipeCreater.load();
-        BlockManager.init();
-        ItemManager.init(event.getSide().isClient());
-    }
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		ItemRecipeCreater.load();
+		BlockManager.init();
+		ItemManager.init(event.getSide().isClient());
+	}
 
-    /**
-     * 初期化。
-     * @param event イベント。
-     */
-    @EventHandler
-    public void init(FMLPreInitializationEvent event) {
-        MinecraftForge.EVENT_BUS.register(PlayerDataEvents.class);
-    }
+	/**
+	 * 初期化。
+	 * @param event イベント。
+	 */
+	@EventHandler
+	public void init(FMLPreInitializationEvent event) {
+		MinecraftForge.EVENT_BUS.register(PlayerDataEvents.class);
+	}
 
-    @EventHandler
-    public void serverLoad(FMLServerStartingEvent event) {
-        //event.registerServerCommand(new DebugCommand());
-    }
+	@EventHandler
+	public void serverLoad(FMLServerStartingEvent event) {
+		event.registerServerCommand(new DeleteRecipeTmpCommand());
+	}
 }
