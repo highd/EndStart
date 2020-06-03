b85788a91da3b11135c6aed4191160d726589ae0
highd <hdgames123@yahoo.co.jp>
2020-06-03 23:26:27 +0900

    新レシピ作成アイテムの追加

---
+++ src/main/java/com/highd120/endstart/item/ItemNewRecipeCreater.java
@@ -0,0 +1,7 @@
+package com.highd120.endstart.item;
+
+import com.highd120.endstart.util.item.ItemRegister;
+
+@ItemRegister(name = "new_recipe")
+public class ItemNewRecipeCreater extends ItemBase {
+}
