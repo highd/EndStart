6f25eab531dd74d7c16afc398b2c991f26dd2c52
highd <hdgames123@yahoo.co.jp>
2020-06-08 22:21:57 +0900

    火と水のオーブを追加

--- src/main/java/com/highd120/endstart/item/ItemExtra.java
+++ src/main/java/com/highd120/endstart/item/ItemExtra.java
@@ -6,7 +6,7 @@ import com.highd120.endstart.util.item.ItemRegister;
 public class ItemExtra extends ItemHasMeta {
 
 	public ItemExtra() {
-		super(new String[] { "ex_core", "life_core" });
+		super(new String[] { "ex_core", "life_core", "fire", "water" });
 	}
 
 }
