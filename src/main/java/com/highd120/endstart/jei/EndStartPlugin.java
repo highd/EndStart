278092de2bc4745d3a5ffddc6c84c5b5f48173b4
highd <hdgames123@yahoo.co.jp>
2020-06-08 23:25:41 +0900

    JEI対応

--- src/main/java/com/highd120/endstart/jei/EndStartPlugin.java
+++ src/main/java/com/highd120/endstart/jei/EndStartPlugin.java
@@ -1,8 +1,11 @@
 package com.highd120.endstart.jei;
 
+import com.highd120.endstart.block.BlockCrafterCore;
 import com.highd120.endstart.block.BlockEndBed;
+import com.highd120.endstart.block.InjectionRecipe;
 import com.highd120.endstart.util.item.ItemManager;
 
+import mezz.jei.api.IJeiHelpers;
 import mezz.jei.api.IJeiRuntime;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.IModRegistry;
@@ -26,6 +29,12 @@ public class EndStartPlugin implements IModPlugin {
 	public void register(IModRegistry registry) {
 		IIngredientBlacklist blacklist = registry.getJeiHelpers().getIngredientBlacklist();
 		blacklist.addIngredientToBlacklist(ItemManager.getItemStack(BlockEndBed.class));
+        IJeiHelpers jeiHelpers = registry.getJeiHelpers();
+        registry.addRecipeCategories(new InjectionCategory(jeiHelpers.getGuiHelper()));
+        registry.addRecipeHandlers(new InjectionHandler());
+        registry.addRecipes(InjectionRecipe.recipes);
+        registry.addRecipeCategoryCraftingItem(ItemManager.getItemStack(BlockCrafterCore.class),
+                InjectionCategory.UID);
 	}
 
 	@Override
