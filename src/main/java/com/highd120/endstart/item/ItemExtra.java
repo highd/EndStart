aca7459d13409d9845373c2de81a49d70b34ba10
highd <hdgames123@yahoo.co.jp>
2020-08-17 10:58:37 +0900

    中間素材の追加

--- src/main/java/com/highd120/endstart/item/ItemExtra.java
+++ src/main/java/com/highd120/endstart/item/ItemExtra.java
@@ -6,7 +6,24 @@ import com.highd120.endstart.util.item.ItemRegister;
 public class ItemExtra extends ItemHasMeta {
 
 	public ItemExtra() {
-		super(new String[] { "ex_core", "life_core", "fire", "water", "shulker_shell" });
+		super(new String[] { 
+			"ex_core", 
+			"life_core", 
+			"fire", 
+			"water", 
+			"shulker_shell", 
+			"magnesium",
+			"crystal",
+			"zinc",
+			"tungsten",
+			"dark_iron", 
+			"platinum",
+			"palladium",
+			"nickel",
+			"volcanic",
+			"carbon",
+			"mercury",
+			"uranium"});
 	}
 
 }
