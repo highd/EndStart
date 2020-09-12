6af0993da2a9c26e6038cccbe13a1848244981b9
highd <hdgames123@yahoo.co.jp>
2020-09-12 23:34:33 +0900

    エンドストーンのツルハシの追加

---
+++ src/main/java/com/highd120/endstart/item/ItemEndPickaxe.java
@@ -0,0 +1,46 @@
+package com.highd120.endstart.item;
+
+import java.util.Random;
+
+import com.highd120.endstart.EndStartCreativeTab;
+import com.highd120.endstart.util.ItemUtil;
+import com.highd120.endstart.util.item.ItemManager;
+import com.highd120.endstart.util.item.ItemRegister;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+@ItemRegister(name = "end_pickaxe")
+public class ItemEndPickaxe extends ItemBase {
+	protected static final Random random = new Random();
+	public ItemEndPickaxe() {
+        setCreativeTab(EndStartCreativeTab.INSTANCE);
+        this.maxStackSize = 1;
+        this.setMaxDamage(299);
+	}
+
+    @Override
+    public float getDestroySpeed(ItemStack stack, IBlockState state) {
+        if (state.getBlock() == Blocks.END_STONE) {
+            return 400;
+        }
+        return super.getDestroySpeed(stack, state);
+    }
+
+    @Override
+    public boolean onBlockDestroyed(ItemStack stack, World worldIn, IBlockState state, BlockPos pos,
+    		EntityLivingBase entityLiving) {
+        if (!worldIn.isRemote && (double)state.getBlockHardness(worldIn, pos) != 0.0D) {
+            stack.damageItem(1, entityLiving);
+        }
+        ItemStack dust = ItemManager.getItemStack(ItemExtra.class, 17);
+        dust.setCount(random.nextInt(4) + 4);
+        ItemUtil.dropItem(worldIn, pos, dust);
+    	return super.onBlockDestroyed(stack, worldIn, state, pos, entityLiving);
+    }
+
+}
