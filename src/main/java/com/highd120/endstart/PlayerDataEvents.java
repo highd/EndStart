3f624eb6ab3ae95f1dc6b95457c840af8d012077
highd <hdgames123@yahoo.co.jp>
2020-09-22 23:31:16 +0900

    スポーン場所を離島に変更

--- src/main/java/com/highd120/endstart/PlayerDataEvents.java
+++ src/main/java/com/highd120/endstart/PlayerDataEvents.java
@@ -12,6 +12,7 @@ import com.highd120.endstart.util.ItemUtil;
 import com.highd120.endstart.util.item.ItemManager;
 
 import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.boss.EntityDragon;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.monster.EntityShulker;
@@ -26,9 +27,12 @@ import net.minecraft.util.EntitySelectors;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldProviderEnd;
 import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.gen.feature.WorldGenEndIsland;
 import net.minecraft.world.gen.feature.WorldGenEndPodium;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
@@ -48,6 +52,84 @@ public class PlayerDataEvents {
 	public static final PropertyDirection FACING = PropertyDirection.create("facing",
 			EnumFacing.Plane.HORIZONTAL);
 
+	
+    private static Chunk getChunk(World worldIn, Vec3d vec3) {
+        return worldIn.getChunkFromChunkCoords(MathHelper.floor(vec3.x / 16.0D), MathHelper.floor(vec3.z / 16.0D));
+    }
+    
+    private static BlockPos findSpawnpointInChunk(Chunk chunkIn) {
+        BlockPos blockpos = new BlockPos(chunkIn.x * 16, 30, chunkIn.z * 16);
+        int i = chunkIn.getTopFilledSegment() + 16 - 1;
+        BlockPos blockpos1 = new BlockPos(chunkIn.x * 16 + 16 - 1, i, chunkIn.z * 16 + 16 - 1);
+        BlockPos blockpos2 = null;
+        double d0 = 0.0D;
+
+        for (BlockPos blockpos3 : BlockPos.getAllInBox(blockpos, blockpos1))
+        {
+            IBlockState iblockstate = chunkIn.getBlockState(blockpos3);
+
+            if (iblockstate.getBlock() == Blocks.END_STONE && !chunkIn.getBlockState(blockpos3.up(1)).isBlockNormalCube() && !chunkIn.getBlockState(blockpos3.up(2)).isBlockNormalCube())
+            {
+                double d1 = blockpos3.distanceSqToCenter(0.0D, 0.0D, 0.0D);
+
+                if (blockpos2 == null || d1 < d0)
+                {
+                    blockpos2 = blockpos3;
+                    d0 = d1;
+                }
+            }
+        }
+
+        return blockpos2;
+    }
+    
+    private static BlockPos findHighestBlock(World p_184308_0_, BlockPos p_184308_1_, int p_184308_2_, boolean p_184308_3_) {
+        BlockPos blockpos = null;
+
+        for (int i = -p_184308_2_; i <= p_184308_2_; ++i) {
+            for (int j = -p_184308_2_; j <= p_184308_2_; ++j) {
+                if (i != 0 || j != 0 || p_184308_3_) {
+                    for (int k = 255; k > (blockpos == null ? 0 : blockpos.getY()); --k) {
+                        BlockPos blockpos1 = new BlockPos(p_184308_1_.getX() + i, k, p_184308_1_.getZ() + j);
+                        IBlockState iblockstate = p_184308_0_.getBlockState(blockpos1);
+
+                        if (iblockstate.isBlockNormalCube() && (p_184308_3_ || iblockstate.getBlock() != Blocks.BEDROCK)) {
+                            blockpos = blockpos1;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return blockpos == null ? p_184308_1_ : blockpos;
+    }
+    
+    private static BlockPos calculateSpawnPoint(World worldIn) {
+    	BlockPos exitPortal;
+        Vec3d vec3d = (new Vec3d(1, 0.0D, 0)).normalize();
+        Vec3d vec3d1 = vec3d.scale(1024.0D);
+
+        for (int i = 16; getChunk(worldIn, vec3d1).getTopFilledSegment() > 0 && i-- > 0; vec3d1 = vec3d1.add(vec3d.scale(-16.0D))) {
+        }
+
+        for (int j = 16; getChunk(worldIn, vec3d1).getTopFilledSegment() == 0 && j-- > 0; vec3d1 = vec3d1.add(vec3d.scale(16.0D))) {
+        }
+
+        Chunk chunk = getChunk(worldIn, vec3d1);
+        exitPortal = findSpawnpointInChunk(chunk);
+
+        if (exitPortal == null) {
+            exitPortal = new BlockPos(vec3d1.x + 0.5D, 75.0D, vec3d1.z + 0.5D);
+            (new WorldGenEndIsland()).generate(worldIn, new Random(exitPortal.toLong()), exitPortal);
+        }
+        exitPortal = findHighestBlock(worldIn, exitPortal, 16, true);
+        return exitPortal;
+    }
+    
+    public static void createPlace(World world, BlockPos point) {
+    	
+    }
 	/**
 	 * プレイヤーの更新処理。
 	 * @param event イベントデータ
@@ -74,9 +156,7 @@ public class PlayerDataEvents {
 					deletePortal(world);
 					createPortalBlock(world, player);
 					persist.setBoolean(TAG_NO_DRAGON, true);
-					BlockPos playerPostion = world
-							.getTopSolidOrLiquidBlock(WorldGenEndPodium.END_PODIUM_LOCATION)
-							.add(1, -3, 1);
+					BlockPos playerPostion = calculateSpawnPoint(world);
 					persist.setInteger("endstart.x", playerPostion.getX());
 					persist.setInteger("endstart.y", playerPostion.getY());
 					persist.setInteger("endstart.z", playerPostion.getZ());
