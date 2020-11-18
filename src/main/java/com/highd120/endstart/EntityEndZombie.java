69e0c6f3f218cc29542dbf5e9eccd4953fd490eb
hdgam <hdgam@DESKTOP-NVVO0P6>
2020-11-19 00:07:20 +0900

    エンドゾンビの弱体化

--- src/main/java/com/highd120/endstart/EntityEndZombie.java
+++ src/main/java/com/highd120/endstart/EntityEndZombie.java
@@ -1,7 +1,11 @@
 package com.highd120.endstart;
 
+import net.minecraft.entity.IEntityLivingData;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.World;
 
 public class EntityEndZombie extends EntityZombie {
@@ -12,5 +16,20 @@ public class EntityEndZombie extends EntityZombie {
 	@Override
 	protected ResourceLocation getLootTable() {
 		return new ResourceLocation(EndStartMain.MOD_ID, "entities/end_zombie");
+	}
+	
+	@Override
+	protected void applyEntityAttributes() {
+        super.applyEntityAttributes();
+        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.1);
+        this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(1.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.ARMOR).setBaseValue(0);
+	}
+	
+	@Override
+	public IEntityLivingData onInitialSpawn(DifficultyInstance difficulty, IEntityLivingData livingdata) {
+        this.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).applyModifier(new AttributeModifier("Random spawn bonus", this.rand.nextGaussian() * 0.05D, 1));
+        this.setLeftHanded(false);
+		return super.onInitialSpawn(difficulty, livingdata);
 	}
 }
