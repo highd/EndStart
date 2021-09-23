4a43239b77a2eb7402ceb58f8f0d38f307fac686
drania <hdgames123@yahoo.co.jp>
2021-09-23 21:51:17 +0900

    テスト用のエネルギー追加

---
+++ src/main/java/com/highd120/endstart/block/BlockEnergy.java
@@ -0,0 +1,22 @@
+package com.highd120.endstart.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+
+public class BlockEnergy extends Block {
+	public BlockEnergy() {
+		super(Material.IRON);
+	}
+	@Override
+	public boolean hasTileEntity(IBlockState state) {
+		return true;
+	}
+	
+	@Override
+	public TileEntity createTileEntity(World world, IBlockState state) {
+		return new TileEnergy();
+	}
+}
